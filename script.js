0d3a3aa0b2d0e077bcb55a7b87d9e01de64100fe
shanergd <shanegaming187@gmail.com>
2026-07-22 09:04:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const no = document.getElementById("no");
+const messageBox = document.getElementById("messageBox");
+const closeButton = document.getElementById("closeButton");
 
 no.addEventListener("click", () => {
-    alert("my message to fcc");
+    messageBox.classList.remove("hidden");
+});
+
+closeButton.addEventListener("click", () => {
+    messageBox.classList.add("hidden");
 });
